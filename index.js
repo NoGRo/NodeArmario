fae9edf77f7df47a218f7d911d6d20aed321c0c4
NoGro <DimNogro@gmai.com>
2019-02-15 02:47:34 -0300

    MEJORANDO BOTONES Y RELAYS

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 var five = require("johnny-five");
 var mqtt = require("mqtt");
 
-var intervalSensors  = 3 * (60*1000); 
-var inTopics = 'Armario/Relays/#';
-var responseTopic = 'response_to_client';
-var relayPins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var intervalSensors = 3 * (60 * 1000);
+var inTopics = 'Armario/Relay/#';
+
+var soilPin = "a1";
+var relayPins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 var relays = [];
+var buttonPins = [12, 13];
+var buttons = {};
+
 
 myBoard = new five.Board();
 myBoard.on("ready", function () {
 
+    var soil = new five.Sensor(soilPin);
 
-    var soil = new five.Sensor("a1");
-    
     var dht22 = new five.Multi({
         controller: "DHT22_I2C_NANO_BACKPACK",
         pin: 11,
     });
-    
+
+    buttonPins.forEach(pinNumber => {
+        buttons[pinNumber] = five.Button(pinNumber);
+    });
+
     relayPins.forEach(pinNumber => {
         relays[pinNumber] = five.Relay(pinNumber);
     });
@@ -27,29 +34,39 @@ myBoard.on("ready", function () {
     var client = mqtt.connect(options);
     client.on('connect', function () {
         client.subscribe(inTopics);
-
+        informarSensores();
         setInterval(informarSensores, intervalSensors);
     });
 
-
+    const events = ["hold", "press", "up"];
+    for (const key in buttons) {
+        const button = buttons[key];
+        events.forEach(event => {
+            button.on(event, () => {
+                client.publish("Armario/Boton/" + event + "/" + this.pin, this.downValue);
+            });
+        });
+    }
 
     client.on('message', (topic, payload) => {
-        //'Armario/Relays/#';
-        let pinNumber = parseInt(topic.split("/")[2]);
-        if (!relays[pinNumber])
-            return;
+        if (topic.startsWith("Armario/Relay/")) {
+            let pinNumber = parseInt(topic.split("/")[2]);
+            let relay = relays[pinNumber];
+            if (!relay)
+                return;
 
-        var message = payload.toString();
+            var message = payload.toString();
+            if (message === "1")
+                relay.on();
+            else if (message === "0")
+                relay.off();
+        }
 
-        if (message === "1")
-            relays[pinNumber].open();
-        else if (message === "0")
-            relays[pinNumber].close();
     });
-
-    function informarSensores{
-        client.publish("Armario/Temperatura",dht22.thermometer.celsius);
-        client.publish("Armario/Humedad",dht22.hygrometer.relativeHumidity);
-        client.publish("Armario/HumedadSuelo",soil.value);
+    function informarSensores() {
+        client.publish("Armario/Temperatura", dht22.thermometer.celsius);
+        client.publish("Armario/Humedad", dht22.hygrometer.relativeHumidity);
+        client.publish("Armario/HumedadSuelo", soil.value);
     }
+
 });
